fa56385467addb43315e747cb51b1c769f96f0cb
Jake Weinstein <jake@aospa.co>
2019-10-01 04:46:59 +0200

    surya: Use speed tuning for performance critical applications
    The default ART setting is quicken: run DEX code verification and
    optimize some DEX instructions to get better interpreter performance.
    
    This commit switches performance critical applications to speed:
    run DEX code verification and AOT-compile all methods.
    
    PRODUCT_DEXPREOPT_SPEED_APPS (New in Android O)
    List of applications that have been identified as core to the products
    and which are desirable to compile with the speed compiler filter.  For
    example, persistent apps such as SystemUI get a chance to use
    profile-guided compilation only at the next reboot, so it may be better
    for the product to have these apps always AOT-compiled.
    
    Change-Id: Ib2dc9a55dfb332ba3fb84a13544d564a63f37ad2
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- device.mk
+++ device.mk
@@ -332,6 +332,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SettingsGoogle \
+    SystemUIGoogle
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
