3f05492b8f22971118980632622e66a91a3357c5
Unicote <arvogl874@gmail.com>
2021-09-09 18:28:56 +0300

    surya: Switch to PowerStats from sunfish
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- device.mk
+++ device.mk
@@ -307,7 +307,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.pixel
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
