7de20b80372f27fe8c8150c9a7f67c9ed3169f5b
Unicote <arvogl874@gmail.com>
2021-09-23 21:24:05 +0300

    surya: Import vendor.qti.hardware.vibrator@1.2-service vibrator HAL
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- device.mk
+++ device.mk
@@ -406,7 +406,7 @@ $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
