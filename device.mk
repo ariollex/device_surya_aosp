dad766b1de783af1647a69b128da07e7d9d4dd05
Unicote <arvogl874@gmail.com>
2021-10-12 17:36:09 +0300

    surya: Switch to prebuilt vibrator HAL
    * To work on all kernels for surya
    
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- device.mk
+++ device.mk
@@ -405,10 +405,6 @@ PRODUCT_COPY_FILES += \
 # Utils
 $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
