713f35043fd1b669c6b47a3bb70eda347e8aea2d
Ariollex <ariollex@gmail.com>
2022-08-08 12:19:11 +0300

    surya: power-libperfmgr: Import from hardware/xiaomi
    HEAD 8fb3a9d7221cee2586ac1d63c79133546dfe2368

--- device.mk
+++ device.mk
@@ -357,7 +357,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
