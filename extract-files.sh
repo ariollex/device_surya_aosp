636542f1d8af5c01cbcd033811e6848713d34919
Ariollex <ariollex@gmail.com>
2022-08-09 15:32:19 +0300

    surya: Rewrite extract scripts for second proprietary files

--- extract-files.sh
+++ extract-files.sh
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2020 Raphielscape LLC. and Haruka LLC.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,18 +12,28 @@ set -e
 DEVICE=surya
 VENDOR=xiaomi
 
+PROPRIETARY_FILES=proprietary-files.txt
+PROPRIETARY_FILES_OTHER=proprietary-files-flame.txt
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+HENTAI_ROOT="${MY_DIR}/../../.."
 
-HELPER="${ANDROID_ROOT}/vendor/hentai/build/tools/extract_utils.sh"
+HELPER="${HENTAI_ROOT}/vendor/hentai/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
+
+# Path to dumps
+FIRST_LINE=$(sed -n '1p' ${PROPRIETARY_FILES}) && PACKAGE_NAME=${FIRST_LINE//# Blobs extracted from }
+FIRST_LINE_OTHER=$(sed -n '1p' ${PROPRIETARY_FILES_OTHER}) && PACKAGE_NAME_OTHER=${FIRST_LINE_OTHER//# Blobs extracted from }
+UNIFIED_PATH="${HENTAI_ROOT}/../dumpyara/working"
+SRC1="${UNIFIED_PATH}/${PACKAGE_NAME}"
+SRC2="${UNIFIED_PATH}/${PACKAGE_NAME_OTHER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
@@ -43,7 +54,6 @@ while [ "${#}" -gt 0 ]; do
                 CLEAN_VENDOR=false
                 ;;
         * )
-                SRC="${1}"
                 ;;
     esac
     shift
@@ -62,9 +72,9 @@ function blob_fixup() {
 }
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${HENTAI_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-extract "${MY_DIR}/proprietary-files-flame.txt" "${SRC2}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/${PROPRIETARY_FILES}" "${SRC1}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/${PROPRIETARY_FILES_OTHER}" "${SRC2}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2020 Raphielscape LLC. and Haruka LLC.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,13 +12,16 @@ set -e
 DEVICE=surya
 VENDOR=xiaomi
 
+PROPRIETARY_FILES=proprietary-files.txt
+PROPRIETARY_FILES_OTHER=proprietary-files-flame.txt
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+HENTAI_ROOT="${MY_DIR}/../../.."
 
-HELPER="${ANDROID_ROOT}/vendor/hentai/build/tools/extract_utils.sh"
+HELPER="${HENTAI_ROOT}/vendor/hentai/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -25,13 +29,14 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${HENTAI_ROOT}"
 
 # Warning headers and guards
 write_headers
 
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-write_makefiles "${MY_DIR}/proprietary-files-flame.txt" true
+write_makefiles "${MY_DIR}/${PROPRIETARY_FILES}" true
+write_makefiles "${MY_DIR}/${PROPRIETARY_FILES_OTHER}" true
 
 # Finish
 write_footers
+$SHELL
